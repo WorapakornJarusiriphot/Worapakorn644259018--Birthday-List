6db9c789cf42670fab1ef79fbb98d0b7a026f357
worapakorn <pupupuwora@gmail.com>
2023-09-04 12:17:52 +0700

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const data = [
+    {
+        id:1,
+        name: "Bertie Yates",
+        age: 29,
+        image: "https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=386&q=80"
+    },
+    {
+        id:2,
+        name: "Hester Hogan",
+        age: 32,
+        image: "https://plus.unsplash.com/premium_photo-1664203067979-47448934fd97?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+    },
+    {
+        id:3,
+        name: "Larry Little",
+        age: 36,
+        image: "https://images.unsplash.com/photo-1555952517-2e8e729e0b44?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80"
+    },
+    {
+        id:4,
+        name: "Sean Walsh",
+        age: 34,
+        image: "https://images.unsplash.com/photo-1545996124-0501ebae84d0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80"
+    },
+    {
+        id:5,
+        name: "Lola Gardner",
+        age: 29,
+        image: "https://images.unsplash.com/photo-1601412436009-d964bd02edbc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80"
+    }
+];
